2df35c2c8be162dfef7ad77a8fb9ad90cb8e477e
caioserrado <caioserrado16@gmail.com>
2021-04-21 16:17:58 -0300

    Introdução a componentes

---
+++ src/components/Multi.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Comp() {
+    return <Text>Comp #Oficial</Text>
+}
+
+function Comp1() {
+    return <Text>Comp #01</Text>
+}
+
+function Comp2() {
+    return <Text>Comp #02</Text>
+}
+
+export { Comp1, Comp2 }
+
+
