c6a7883a19b674a4f86d713846779b030a086b3f
caioserrado <caioserrado16@gmail.com>
2021-04-22 16:22:30 -0300

    Exemplo comunicação indireta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
-import Pai from './components/direta/Pai'
+import Pai from './components/indireta/Pai'
+//import Pai from './components/direta/Pai'
 import Contator from './components/Contator'
 import Botao from './components/Botao'
 import Titulo from './components/Titulo'
@@ -12,8 +13,9 @@ import Primeiro from './components/Primeiro'
 
 export default () => (
     <View style={style.App}>
-        <Pai/>
+        <Pai />
         {/* 
+        <Pai/>
         <Contator inicial={100} passo={13} />
         <Contator />
         <Botao/>
